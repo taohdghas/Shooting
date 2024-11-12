2ef91b5f662fd55d1c1bcbe4a3549f01d1f1e12b
taohdghas <143993457+taohdghas@users.noreply.github.com>
2024-11-12 15:26:20 +0900

    作業途中

---
+++ project/Camera.cpp
@@ -0,0 +1,27 @@
+#include "Camera.h"
+#include "Math.h"
+
+//デフォルトコンストラクタ
+Camera::Camera()
+	:transform({ {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f},{0.0f,0.0f,0.0f} })
+	, FovY(0.45f)
+	, aspectRatio((WindowsAPI::kClientWitdh) / float(WindowsAPI::kClientHeight))
+	, nearClip(0.1f)
+	,farClip(100.0f)
+	, worldMatrix(Math::MakeAffineMatrix(transform.scale, transform.rotate, transform.translate))
+	, viewMatrix(Math::Inverse(worldMatrix))
+	, projectionMatrix(Math::MakePerspectiveFovMatrix(FovY, aspectRatio, nearClip, farClip))
+	, viewProjectionMatrix(Math::Multiply(viewMatrix, projectionMatrix))
+{}
+
+//更新
+void Camera::Update() {
+	//ワールド行列
+	worldMatrix;
+	//ビュー行列
+	viewMatrix;
+	//プロジェクション行列
+	projectionMatrix;
+	//合成行列
+	viewProjectionMatrix;
+}
--- project/engine/3d/Object3dBase.h
+++ project/engine/3d/Object3dBase.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "DirectXBase.h"
+#include "Camera.h"
 
 //3Dオブジェクト共通部
 class Object3dBase
@@ -13,13 +14,17 @@ public:
 	void GenerategraphicsPipeline();
 	//共通描画設定
 	void DrawBaseSet();
+	///setter///
+	void SetDefaultCamera(Camera* camera) { this->defaultCamera = camera; }
 	///getter///
 	DirectXBase* GetDxBase()const { return directxBase_; }
-
+	Camera* GetDefaultCamera()const { return defaultCamera; }
 private:
 	HRESULT hr;
 	//DirectXBaseのポインタ
 	DirectXBase* directxBase_;
+	//Cameraポインタ
+	Camera* defaultCamera = nullptr;
 	//ルートシグネチャ
 	Microsoft::WRL::ComPtr<ID3D12RootSignature> rootSignature;
 	//inputlayout
