368c60cdc975ee849276b79803a449b3a5574c5f
taohdghas <143993457+taohdghas@users.noreply.github.com>
2025-01-06 22:05:27 +0900

    途中

--- project/engine/manager/ImGuiManager.cpp
+++ project/engine/manager/ImGuiManager.cpp
@@ -1,8 +1,10 @@
 #include "ImGuiManager.h"
 #include "imgui_impl_win32.h"
+#include "imgui_impl_dx12.h"
 
 //初期化
 void ImGuiManager::Initialize(WindowsAPI* windowsAPI,DirectXBase* directxBase){
+	directbase_ = directxBase;
 	//ImGuiのコンテキストを生成
 	ImGui::CreateContext();
 	//ImGuiのスタイルを設定
@@ -10,17 +12,9 @@ void ImGuiManager::Initialize(WindowsAPI* windowsAPI,DirectXBase* directxBase){
 	//win32用初期化
 	ImGui_ImplWin32_Init(windowsAPI->GetHwnd());
 	//////DirectX12用初期化//////
-	
 	//DierctX12デバイス
 	ImGui_ImplWin32_Init(directxBase->Getdevice().Get());
 	//バックバッファの数
 	ImGui_ImplWin32_Init(directxBase->);
-	//RTVのフォーマット
-
-	//SRV用デスクリプタヒープ
-
-	//SRV用のCPUハンドル
-
-	//SRV用のGPUハンドル
 
 }
--- project/engine/manager/ImGuiManager.h
+++ project/engine/manager/ImGuiManager.h
@@ -6,5 +6,7 @@ class ImGuiManager
 public:
 	//初期化
 	void Initialize(WindowsAPI*windowsAPI,DirectXBase*directxBase);
+private:
+	DirectXBase* directbase_;
 };
 
