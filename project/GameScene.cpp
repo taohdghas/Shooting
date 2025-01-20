e3efbdf5f6f9b253ca17fc10b0bc6c5518b580dc
taohdghas <143993457+taohdghas@users.noreply.github.com>
2025-01-20 11:10:32 +0900

    クラス作成まで

---
+++ project/GameScene.cpp
@@ -0,0 +1,21 @@
+#include "GameScene.h"
+
+//初期化
+void GameScene::Initialize() {
+
+}
+
+//終了
+void GameScene::Finalize() {
+
+}
+
+//更新
+void GameScene::Update() {
+
+}
+
+//描画
+void GameScene::Draw() {
+
+}
---
+++ project/GameScene.h
@@ -0,0 +1,16 @@
+#pragma once
+class GameScene
+{
+public:
+	//初期化
+	void Initialize();
+	//終了
+	void Finalize();
+	//更新
+	void Update();
+	//描画
+	void Draw();
+private:
+
+};
+
