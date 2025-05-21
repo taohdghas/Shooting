252927d67aee5aa439d2b5722f17cc2dee6ea5c4
taohdghas <143993457+taohdghas@users.noreply.github.com>
2025-05-21 15:22:24 +0900

    多分5x5もできた

--- project/resources/shaders/BoxFilter.PS.hlsl
+++ project/resources/shaders/BoxFilter.PS.hlsl
@@ -21,17 +21,53 @@ static const float kKernel3x3[3][3] =
     { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
     { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
 };
+/*
+static const float2 kIndex5x5[5][5] =
+{
+    { { -2.0f, -2.0f }, { -1.0f, -2.0f }, { 0.0f, -2.0f }, { 1.0f, -2.0f }, { 2.0f, -2.0f } },
+    { { -2.0f, -1.0f }, { -1.0f, -1.0f }, { 0.0f, -1.0f }, { 1.0f, -1.0f }, { 2.0f, -1.0f } },
+    { { -2.0f, 0.0f }, { -1.0f, 0.0f }, { 0.0f, 0.0f }, { 1.0f, 0.0f }, { 2.0f, 0.0f } },
+    { { -2.0f, 1.0f }, { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f }, { 2.0f, 1.0f } },
+    { { -2.0f, 2.0f }, { -1.0f, 2.0f }, { 0.0f, 2.0f }, { 1.0f, 2.0f }, { 2.0f, 2.0f } },
+};
 
+static const float kKernel5x5[5][5] =
+{
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+};
+*/
 PixelShaderOutput main(VertexShaderOutput input)
 {
     //uvStepSizeの算出
     uint width, height;
     gTexture.GetDimensions(width, height);
-    float2 uvStepSize = float2(rcp((float) width), rcp((float)height));
+    float2 uvStepSize = float2(rcp((float) width), rcp((float) height));
     
     PixelShaderOutput output;
     output.color.rgb = float3(0.0f, 0.0f, 0.0f);
     output.color.a = 1.0f;
+    
+    /*
+    //5x5ループ
+    for (int y = 0; y < 5; ++y)
+    {
+        for (int x = 0; x < 5; ++x)
+        {
+            //現在のtexcoordを算出
+            float2 texcoord = input.texcoord + kIndex5x5[y][x] * uvStepSize;
+            //色に1/9掛けて足す
+            float3 fetchColor = gTexture.Sample(gSampler, texcoord).rgb;
+            output.color.rgb += fetchColor * kKernel5x5[y][x];
+        }
+
+    }
+    */
+    
+    
     //3x3ループ
     for (int x = 0; x < 3; ++x)
     {
@@ -44,6 +80,6 @@ PixelShaderOutput main(VertexShaderOutput input)
             output.color.rgb += fetchColor * kKernel3x3[x][y];
         }
     }
- 
+    
     return output;
 }
