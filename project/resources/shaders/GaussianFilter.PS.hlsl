72c19175b66aa0ab0f41dbba859bafa754cb3b78
taohdghas <143993457+taohdghas@users.noreply.github.com>
2025-05-21 16:04:22 +0900

    途中まで

---
+++ project/resources/shaders/GaussianFilter.PS.hlsl
@@ -0,0 +1,107 @@
+#include "Fullscreen.hlsli"
+
+Texture2D<float4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
+
+static const float PI = 3.14159265f;
+
+struct PixelShaderOutput
+{
+    float4 color : SV_Target0;
+};
+
+static const float2 kIndex3x3[3][3] =
+{
+    { { -1.0f, -1.0f }, { 0.0f, -1.0f }, { 1.0f, -1.0f } },
+    { { -1.0f, 0.0f }, { 0.0f, 0.0f }, { 1.0f, 0.0f } },
+    { { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f } },
+};
+
+static const float kKernel3x3[3][3] =
+{
+    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
+    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
+    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
+};
+/*
+static const float2 kIndex5x5[5][5] =
+{
+    { { -2.0f, -2.0f }, { -1.0f, -2.0f }, { 0.0f, -2.0f }, { 1.0f, -2.0f }, { 2.0f, -2.0f } },
+    { { -2.0f, -1.0f }, { -1.0f, -1.0f }, { 0.0f, -1.0f }, { 1.0f, -1.0f }, { 2.0f, -1.0f } },
+    { { -2.0f, 0.0f }, { -1.0f, 0.0f }, { 0.0f, 0.0f }, { 1.0f, 0.0f }, { 2.0f, 0.0f } },
+    { { -2.0f, 1.0f }, { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f }, { 2.0f, 1.0f } },
+    { { -2.0f, 2.0f }, { -1.0f, 2.0f }, { 0.0f, 2.0f }, { 1.0f, 2.0f }, { 2.0f, 2.0f } },
+};
+
+static const float kKernel5x5[5][5] =
+{
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+};
+*/
+
+float gauss(float x, float y, float sigma)
+{
+    float exponent = -(x * x + y * y) * rcp(2.0f * sigma * sigma);
+    float denominator = 2.0f * PI * sigma * sigma;
+    return exp(exponent) * rcp(denominator);
+}
+
+PixelShaderOutput main(VertexShaderOutput input)
+{
+    float weight = 0.0f;
+    float kernel3x3[3][3];
+    for (int x = 0; x < 3; ++x)
+    {
+        for (int y = 0; y < 3; ++y)
+        {
+            kernel3x3[x][y] = gauss(kIndex3x3[x][y].x, kIndex3x3[x][y].y, 2.0f);
+            weight += kernel3x3[x][y];
+        }
+
+    }
+    
+    
+    //uvStepSizeの算出
+     uint width, height;
+    gTexture.GetDimensions(width, height);
+    float2 uvStepSize = float2(rcp((float) width), rcp((float) height));
+    
+    PixelShaderOutput output;
+    output.color.rgb = float3(0.0f, 0.0f, 0.0f);
+    output.color.a = 1.0f;
+    
+    /*
+    //5x5ループ
+    for (int y = 0; y < 5; ++y)
+    {
+        for (int x = 0; x < 5; ++x)
+        {
+            //現在のtexcoordを算出
+            float2 texcoord = input.texcoord + kIndex5x5[y][x] * uvStepSize;
+            //色に1/9掛けて足す
+            float3 fetchColor = gTexture.Sample(gSampler, texcoord).rgb;
+            output.color.rgb += fetchColor * kKernel5x5[y][x];
+        }
+
+    }
+    */
+    
+    //3x3ループ
+    for (int x = 0; x < 3; ++x)
+    {
+        for (int y = 0; y < 3; ++y)
+        {
+            //現在のtexcoordを算出
+            float2 texcoord = input.texcoord + kIndex3x3[x][y] * uvStepSize;
+            //色に1/9掛けて足す
+            float3 fetchColor = gTexture.Sample(gSampler, texcoord).rgb;
+            output.color.rgb += fetchColor * kKernel3x3[x][y];
+        }
+    }
+    
+    return output;
+}
