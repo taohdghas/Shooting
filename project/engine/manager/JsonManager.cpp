79222a0e44f9db930382c2e959be58c645b90e66
taohdghas <143993457+taohdghas@users.noreply.github.com>
2025-05-23 12:50:01 +0900

    途中保存

--- project/engine/manager/JsonManager.cpp
+++ project/engine/manager/JsonManager.cpp
@@ -1,4 +1,8 @@
 #include "JsonManager.h"
+#include <string>
+#include <fstream>
+#include <cassert>
+#include <json.hpp>
 
 JsonManager* JsonManager::instance = nullptr;
 
@@ -8,4 +12,41 @@ JsonManager* JsonManager::GetInstance() {
 		instance = new JsonManager;
 	}
 	return instance;
+}
+
+//Jsonファイルの読み込み
+void JsonManager::LoadJsonFile() {
+	//連続してフルパスを得る
+	const std::string fullpath =;
+
+	//ファイルストリーム
+	std::ifstream file;
+
+	//ファイルを開く
+	file.open(fullpath);
+	//ファイルオープン失敗をチェック
+	if (file.fail()) {
+		assert(0);
+	}
+
+	//JSON文字列から解凍したデータ
+	nlohmann::json deserialized;
+
+	//解凍
+	file >> deserialized;
+
+	//正しいレベルデータファイルかチェック
+	assert(deserialized.is_object());
+	assert(deserialized.contains("name"));
+	assert(deserialized["name"].is_string());
+
+	//"name"を文字列として取得
+	std::string name =
+		deserialized["name"].get<std::string>();
+	//正しいレベルデータファイルかチェック
+	assert(name.compare("scene") == 0);
+
+	//レベルデータ格納用インスタンスを生成
+	
+
 }
