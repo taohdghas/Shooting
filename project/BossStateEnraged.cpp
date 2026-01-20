f2b8b0941443369cecf6fbd9640803b7d0aaf460
taohdghas <143993457+taohdghas@users.noreply.github.com>
2026-01-20 10:29:08 +0900

    強化状態で通常攻撃が抜けていたので追加

--- project/BossStateEnraged.cpp
+++ project/BossStateEnraged.cpp
@@ -17,11 +17,23 @@ void BossStateEnraged::Update(Boss1& boss) {
 	//移動
     boss.Move();
 
-	// 二段高さショット発射処理
+    // 二段高さショット発射処理
+    if (boss.UpdateDelayTimer(fire_timer_, fire_interval_current_)) {
+        boss.FireDoubleHeightShot();
+        is_second_shot_pending_ = true;
+        fire_interval_current_ = boss.DecideFireInterval();
+    }
+    // 二段ショット遅延処理
+    if (is_second_shot_pending_ &&
+        boss.UpdateDelayTimer(second_shot_delay_timer_, kSecondShotDelay)) {
+        boss.FireDoubleHeightShot();
+        is_second_shot_pending_ = false;
+    }
+	// 扇状ショット発射処理
     if (!isFanShotPending_) {
         isFanShotPending_ = true;
     }
-	// 二段ショット遅延処理
+	// 扇状ショット遅延処理
     if (isFanShotPending_ &&
         boss.UpdateDelayTimer(fanShotDelayTimer_, kFanShotDelay)) {
         boss.FireFanShot();
